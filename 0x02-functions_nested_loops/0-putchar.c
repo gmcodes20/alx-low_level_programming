2c7f779c730a47c15d237b520e006ad9ca6c8a8c
Godwin Martins <gmcodes20@gmail.com>
2022-03-15 14:24:02 +0000

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char test[9]= "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(test[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
