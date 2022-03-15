7cf385068e549648c69e6fad69d03e67249d3db2
Godwin Martins <gmcodes20@gmail.com>
2022-03-15 21:32:11 +0000

    Create 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 
 
 /**
- * print_alphabet - print_alphabet
+ * _islower - print_alphabet
  * @c: its mine damnit
  *
+ * Return: 1 if lowercase and 0 otherwise;
  * Does my stuff explained.
  */
 
