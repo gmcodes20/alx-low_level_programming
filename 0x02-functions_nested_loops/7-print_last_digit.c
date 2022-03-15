d795a1427061c6a0922576b79b7dbbcbb07c2157
Godwin Martins <gmcodes20@gmail.com>
2022-03-15 23:39:40 +0000

    Modify  7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include  "main.h"
 
- /**
-  * print_last_digit - prints the last digit of a given number
-  * @num: its mine damnit
-  *
-  * Return: the last digit of the number provided;
-  * Does my stuff explained.
-  */
-
+/**
+ * print_last_digit - print_alphabet
+ * @num: its mine damnit
+ *
+ * Return: 1 if positive, return -1 if negative and 0 otherwise;
+ * Does my stuff explained.
+ */
 int print_last_digit(int num)
 {
 	int n;
 
 	n = num % 10;
+	_putchar(n);
 	return (n);
 }
