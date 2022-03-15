1a6054a4c0b924e4d6775dda20930695ceca0956
Godwin Martins <gmcodes20@gmail.com>
2022-03-15 18:08:13 +0000

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
+
+print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H_
 
 int _putchar(char c);  /* An example function declaration */
+void print_alphabet(void);
 
 #endif 
