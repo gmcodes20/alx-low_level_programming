f56c775f7ee44fcd6683e4649adf345f466b93b0
Godwin Martins <gmcodes20@gmail.com>
2022-03-15 18:22:47 +0000

    Modify 1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,19 @@
 #ifndef MAIN_H_   /* Include guard */
 #define MAIN_H_
 
-int _putchar(char c);  /* An example function declaration */
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 
 #endif 
