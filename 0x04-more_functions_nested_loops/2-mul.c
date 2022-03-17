3081eee76846e45d0dc037a768690de4a9c7590f
Godwin Martins <gmcodes20@gmail.com>
2022-03-17 21:44:39 +0000

    Modify 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _mul - Multiply two integers
+ * mul - Multiply two integers
  * @a: first integer
  * @b: Second integer
-
  * Return: the result ofthe multiplication
  */
 
