18735f03f5c54f84752d2e5408eb908987d54998
Godwin Martins <gmcodes20@gmail.com>
2022-03-17 22:16:38 +0000

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function print numbers
+ *
+ * return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
