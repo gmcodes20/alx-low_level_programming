b833902f5b6cef5c5e3422a136217f617d5b7bab
Godwin Martins <gmcodes20@gmail.com>
2022-03-25 05:26:48 +0000

    modify 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,5 +12,20 @@
 
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int j;
+
+	if (n < 0)
+	{
+		j = -n;
+		_putchar('-');
+	}
+	else
+	{
+		j = n;
+	}
+	if (j / 10)
+	{
+		print_number(j / 10);
+	}
+	_putchar(j % 10 + '0');
 }
