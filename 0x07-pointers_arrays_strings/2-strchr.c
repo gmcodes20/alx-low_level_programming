079d947ad0cb7250e1a37c0ef48733149a94e72e
Godwin Martins <gmcodes@gmail.com>
2022-03-28 16:57:22 +0100

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strchr - Return a pointer to the first occurence of the character @c
+* @c: the character in question5D5D5
+* @s: the string pointer
+*
+* Return: Pointer to c
+*/
+
+
+
+char *_strchr(char *s, char c)
+{
+	int s_len;
+        int i;
+
+	if (s == NULL)
+	{
+		return NULL;
+	}
+	
+	s_len = strlen(s); 
+	for (i = 0; i < s_len; i++)
+	{
+		if ((char) c == s[i])
+		{
+			return (&s[i]);
+		}
+	}
+	return NULL;
+
+}
