b74ff9895f0473a082135b8eda99de05e9ca6ed3
Godwin Martins <gmcodes@gmail.com>
2022-03-28 15:37:23 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+* _memset -the function
+* @s: Variable 1
+* @b: Variable 2
+* @n: variable 3
+*
+* Return: b
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
