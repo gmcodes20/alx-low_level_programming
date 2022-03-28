a10a69ebeba2ff823c413d1f09f3fb45b0e82d5c
Godwin Martins <gmcodes@gmail.com>
2022-03-28 16:02:22 +0100

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _memcpy - Function that copies memory arrea
+* @dest: Destination
+* @src: Source
+* @n:  max numbers of bytes to copy
+* Return: dest
+*/
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = *src;
+		i++;
+	}
+
+	return (dest);
+}
