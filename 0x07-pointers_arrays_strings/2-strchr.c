9de598474f1c104b124e33ce55d741972474988a
Godwin Martins <gmcodes@gmail.com>
2022-03-28 21:23:44 +0100

    Modify  2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,13 +16,13 @@ char *_strchr(char *s, char c)
 	{
 		if (*s == c)
 		{
-			return (*s);
+			return (s);
 		}
-		s++
+		s++;
 	}
 	if (*s == c)
 	{
-		return (*s);
+		return (s);
 	}
 return (NULL);
 }
