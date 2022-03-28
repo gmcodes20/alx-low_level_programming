a1355b94816fcca04b97b5414a2625c24b90bd95
Godwin Martins <gmcodes@gmail.com>
2022-03-28 22:37:09 +0100

    create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - function calculates the length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept.
+ * @s: Original String
+ * @accept: Caparism
+ *
+ * Return: The numbers of accept found in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b, c;
+
+	a = 0;
+	c = 0;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[a])
+			{
+				c++;
+				break;
+			}
+		b++;
+	}
+	a++;
+	if (accept[b] == '\0')
+		break;
+	}
+
+	return (b);
+}
