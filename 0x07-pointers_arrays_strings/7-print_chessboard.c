fd64ae3baef448ddc1a24363450d58a4b6501154
Godwin Martins <gmcodes@gmail.com>
2022-03-29 04:40:05 +0100

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chess board like output
+ * @a: 2d array of X * X
+ *
+ * Return: void
+ */
+
+
+void print_chessboard(char (*a)[8]);
+{
+	int b,c;
+
+	b = 0;
+	while (b < 8)
+	{
+		c = 0;
+		while (c < 8)
+		{
+			_putchar(a[b][c]);
+			c++
+		}
+		b++;
+		if (a < 8)
+		{
+			_putchar('\n');
+		}
+	}
+}
