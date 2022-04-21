b0b5b89f25f6a5e747c122873cc565f2f9516e63
Godwin Martins <gmcodes@gmail.com>
2022-04-21 22:02:22 +0100

    Modify lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct list_s - Singly Linked List
  * @str: string - (malloc'edstring)
@@ -14,9 +13,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
