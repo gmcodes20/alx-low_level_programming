790d405f3d11ceb7a4d099aeeb37e21ddea4f662
Godwin Martins <gmcodes@gmail.com>
2022-05-02 23:51:40 +0100

    Modify 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 
